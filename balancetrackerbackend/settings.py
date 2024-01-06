f065fd8d7214ec8407d69e5dd38ae1a8a038d2b8
Aunpun283 <151039011+Aunpun283@users.noreply.github.com>
2024-01-06 07:52:14 +0700

    Update settings.py

--- balancetrackerbackend/settings.py
+++ balancetrackerbackend/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s8sag0l($dbe9_sz4%5gba3vb+ue8+=^2sus%qlzy*s9v5mb!u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
